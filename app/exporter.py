af0efe831626806cd945c488a977991877126526
Tim Birkett <57101177+js-timbirkett@users.noreply.github.com>
2020-04-24 15:03:10 +0100

    feat: GET IAM credentials from instance metadata (#4)
    * feat: Get credentials from instance
    
    * feat: Optimise Dockerfile for caching
    
    * fix: Spelling mistake

--- app/exporter.py
+++ app/exporter.py
@@ -59,6 +59,10 @@ class S3Collector(object):
         self._s3config.update_option('use_https', use_https)
         signature = config.get('signature_v2', True)
         self._s3config.update_option('signature_v2', signature)
+
+        if len(self._s3config.access_key)==0:
+            self._s3config.role_config()
+
         self._s3 = S3(self._s3config)
 
     def collect(self):
@@ -88,7 +92,7 @@ class S3Collector(object):
         )
         file_count_gauge = GaugeMetricFamily(
                 's3_file_count',
-                'Numbeer of existing files in folder',
+                'Number of existing files in folder',
                 labels=['folder'],
         )
         for folder in config.get('folders'):
